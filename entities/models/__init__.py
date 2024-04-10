bfe216e0852ea26957739b2357468678eb762b9d
durachok <fenixrnd@mail.ru>
2024-04-10 21:43:55 +0700

    add kiosks

--- entities/models/__init__.py
+++ entities/models/__init__.py
@@ -2,4 +2,6 @@ from .outlet_types import OutletTypes
 from .outlets import Outlets
 from .client import Client
 from .branch import Branch
-__all__ = ['Outlets', 'OutletTypes', 'Client', 'Branch']
+from .kiosk import Kiosk
+
+__all__ = ['Outlets', 'OutletTypes', 'Client', 'Branch', 'Kiosk']
---
+++ entities/models/kiosk.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+from entities.models import Branch
+
+
+class Kiosk(models.Model):
+    outlet = models.OneToOneField(
+        'Outlets',
+        models.DO_NOTHING,
+        primary_key=True,
+        db_comment='Здание где расположен',
+        help_text='Связь со зданием в котором расположен киоск'
+    )
+    branch_outlet = models.ForeignKey(
+        Branch,
+        models.DO_NOTHING,
+        db_comment='Киоск связан с филиаом',
+        help_text='Связь с филиалом, к которому прикреплен киоск'
+    )
+    num_workers = models.IntegerField(
+        db_comment='Количество работников',
+        help_text='Количество работников в киоске'
+    )
+
+    class Meta:
+        db_table = 'kiosks'
+        db_table_comment = 'Киоск'
+        verbose_name = 'Киоск'
+        verbose_name_plural = 'Киоски'
+
+    def __str__(self):
+        return f'{self.outlet.address}'
