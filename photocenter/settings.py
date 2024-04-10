1117222ed9d47db314c64fbd5298a14a25d53015
durachok <fenixrnd@mail.ru>
2024-04-10 15:08:29 +0700

    add postgres connection

--- photocenter/settings.py
+++ photocenter/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'photocenter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_dev',
+        'USER': 'durachok',
+        'PASSWORD': 'cpp123321',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
