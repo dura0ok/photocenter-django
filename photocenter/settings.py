25714c50973d92617b02c7bd315f65021f5f9f06
durachok <fenixrnd@mail.ru>
2024-05-15 18:22:46 +0700

    current state

--- photocenter/settings.py
+++ photocenter/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',se
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
     'entities',
